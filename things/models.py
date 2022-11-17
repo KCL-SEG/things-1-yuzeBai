6566eae9de78192bd150253cea9c013020259958
yuzeBai <1600242974@qq.com>
2022-11-17 10:32:59 +0000

    /Test 9

--- things/models.py
+++ things/models.py
@@ -3,18 +3,22 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
 class Thing(AbstractUser):
+    username = models.CharField(blank = True)
+    password = models.CharField(blank = True)
     name = models.CharField(
         max_length=30,
-        unique=True,
-        blank=False
+        #unique=True,
+        #blank=False
     )
     description = models.CharField(
         max_length=120,
+        blank = True
     )
     quantity = models.IntegerField(
-        #default=1,
-        #validators=[
-        #MaxValueValidator(100),
-        #MinValueValidator(0)
-        #]
+        blank = True,
+        default=1,
+        validators=[
+        MaxValueValidator(100),
+        MinValueValidator(0)
+        ]
     )
