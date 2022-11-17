8d0847516a1939bd3cc2b3151322eeedff74e090
yuzeBai <1600242974@qq.com>
2022-11-17 09:16:16 +0000

    /Test the ex

---
+++ things/models.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import AbstracUser
+from django.db import models
+
+class Thing(AbstractUser):
+    name = models.CharField(
+        max_length=30,
+        unique=True,
+        blank=False
+    )
+    description = models.CharField(
+        max_length=120,
+    )
+    quantity = models.IntegerField(
+        default=1
+        validators=[
+        MaxValueValidator(100),
+        MinValueValidator(0)
+        ]
+    )
