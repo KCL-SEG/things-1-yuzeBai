d04601d4d7c779306065888bf31427f02b40d466
yuzeBai <1600242974@qq.com>
2022-11-17 09:39:02 +0000

    /Test 8

--- things/models.py
+++ things/models.py
@@ -12,9 +12,9 @@ class Thing(AbstractUser):
         max_length=120,
     )
     quantity = models.IntegerField(
-        default=1,
-        validators=[
-        MaxValueValidator(100),
-        MinValueValidator(0)
-        ]
+        #default=1,
+        #validators=[
+        #MaxValueValidator(100),
+        #MinValueValidator(0)
+        #]
     )
